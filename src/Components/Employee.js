54c5fd0547a9db5a1a8894d763c2068aa000aa30
Elton <eltonbleta93@gmail.com>
2022-09-26 15:16:18 +0200

    first commit

---
+++ src/Components/Employee.js
@@ -0,0 +1,24 @@
+const Employee = ({employee}) => {
+  return (
+    <>
+        <td>{employee.name}</td>
+        <td>{employee.email}</td>
+        <td>{employee.address}</td>
+        <td>{employee.phone}</td>
+        <td>
+          <a href="#editEmployeeModal" class="edit" data-toggle="modal">
+            <i class="material-icons" data-toggle="tooltip" title="Edit">
+              &#xE254;
+            </i>
+          </a>
+          <a href="#deleteEmployeeModal" class="delete" data-toggle="modal">
+            <i class="material-icons" data-toggle="tooltip" title="Delete">
+              &#xE872;
+            </i>
+          </a>
+        </td>
+    </>
+  );
+};
+
+export default Employee;
---
+++ src/Components/EmployesList.js
@@ -0,0 +1,53 @@
+import Employee from "./Employee";
+import { useContext } from "react";
+import { EmployeeContext } from "../contexts/EmployeeContext";
+
+const EmployeeList = () => {
+
+    const {employees} = useContext(EmployeeContext);
+
+  return (
+    <>
+      <div className="table-title">
+        <div className="row">
+          <div className="col-sm-6">
+            <h2>
+              Manage<b>Employees</b>
+            </h2>
+          </div>
+          <div className="col-sm-6">
+            <a
+              href="#addEmployeeModal"
+              className="btn btn-success"
+              data-toggle="modal"
+            >
+              <i class="material-icons">&#xE147;</i> <span>Add New Employee</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            {
+                employees.map(employee => (
+                    <tr key={employee.id}>
+                        <Employee employee={employee} />
+                    </tr>
+                ))
+            }
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default EmployeeList;
